0b1af84174e1833c31db33a5deb17bc490d50213
a.firouzpour <a.firouzpour@novin.dev>
2022-06-22 13:02:46 +0430

    feat: :sparkles: pagination

--- src/pages/pokemons/Pokemons.jsx
+++ src/pages/pokemons/Pokemons.jsx
@@ -1,56 +1,95 @@
 import { CircularProgress, Container, Grid, Typography, useTheme } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { usePokemons } from './hooks/PokemonsHook';
 import FindPokemon from './FindPokemon';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { useQueryClient } from 'react-query'
 
 const Pokemons = () => {
-  const {data, isLoading, isError, isFetching, error} = usePokemons();
+  const [url,setUrl] = useState({next:true,previous:null,status:"all"})
+  const {data, isLoading, isError, isFetching, error} = usePokemons(url);
+  const query = useQueryClient();
   const theme = useTheme();
+
+  const prevPage = () => {
+    return new Promise((res,rej)=>{
+      setUrl({...url,status:"prev",previous:data?.previous});
+      res();
+    }).then(()=>{
+      query.invalidateQueries(["pokemons",url])
+    })
+  }
+
+  const nextPage = () => {
+    return new Promise((res,rej)=>{
+      setUrl({...url,status:"next",next:data?.next})
+      res();
+    }).then(()=>{
+      query.invalidateQueries(["pokemons",url])
+    })
+  }
+
+  useEffect(()=>{
+    if(data){
+      if(data.next){
+        setUrl({...url,next:true})
+      }
+      if(data.previous){
+        setUrl({...url,previous:true})
+      }
+    }
+  },[data])
+
   return (
-    <Container>
-      <Grid style={{padding:"0 5.5%"}}>
-        <FindPokemon/>
+    <Container className='d-flex'>
+      {url.previous?<Grid className='d-flex align-items-center'><ArrowBackIosNewIcon onClick={prevPage} /></Grid>:null}
+      <Grid className='d-flex justify-content-center flex-column'>
+        <Grid style={{padding:"0 5.5%"}} className="w-100">
+          <FindPokemon/>
+        </Grid>
+        <Grid container className="d-flex justify-content-center">
+          {isLoading ? <CircularProgress/>: isError ? 
+          <Typography component={"b"} color={"red"}> {error.message} </Typography>
+            : 
+              Object.keys(data).length ? data.results.map((singlePoke,index) => {
+                return (
+                  <Grid 
+                    key={index}
+                    xs={2}
+                    item
+                    minWidth={100}
+                    margin={1}
+                    style={{
+                      textDecoration:"none",
+                      backgroundColor:theme.palette.primary.main,
+                      borderRadius:"5px",
+                      color:theme.palette.secondary.main
+                    }}
+                    className="d-flex justify-content-center align-items-center"
+                  >
+                    <NavLink
+                      to={`/pokemons/${singlePoke.name}`}
+                      state={{url:singlePoke.url}}
+                      style={{textDecoration:"none", color:theme.palette.secondary.main}}
+                      >
+                      <Grid
+                        margin={5}
+                        className="text-center"
+                        key={index}
+                      >
+                        {singlePoke.name.toUpperCase()}
+                      </Grid>
+                    </NavLink>
+                  </Grid>
+                )
+              }):"EMPTY" 
+            }
+            {isFetching?<CircularProgress/>:null}
+        </Grid>
       </Grid>
-      <Grid container className="d-flex justify-content-center">
-        {isLoading ? <CircularProgress/>: isError ? 
-        <Typography component={"b"} color={"red"}> {error.message} </Typography>
-          : 
-            Object.keys(data).length ? data.results.map((singlePoke,index) => {
-              return (
-                <Grid 
-                  key={index}
-                  xs={2}
-                  item
-                  minWidth={100}
-                  margin={1}
-                  style={{
-                    textDecoration:"none",
-                    backgroundColor:theme.palette.primary.main,
-                    borderRadius:"5px",
-                    color:theme.palette.secondary.main
-                  }}
-                  className="d-flex justify-content-center align-items-center"
-                >
-                  <NavLink
-                    to={`/pokemons/${singlePoke.name}`}
-                    state={{url:singlePoke.url}}
-                    style={{textDecoration:"none", color:theme.palette.secondary.main}}
-                    >
-                    <Grid
-                      margin={5}
-                      className="text-center"
-                      key={index}
-                    >
-                      {singlePoke.name.toUpperCase()}
-                    </Grid>
-                  </NavLink>
-                </Grid>
-              )
-            }):"EMPTY" 
-          }
-          {isFetching?<CircularProgress/>:null}
-      </Grid>
+      {url.next?<Grid className='d-flex align-items-center' onClick={nextPage}><ArrowForwardIosIcon/></Grid>:null}
     </Container>
   )
 }
--- src/pages/pokemons/hooks/PokemonsHook.js
+++ src/pages/pokemons/hooks/PokemonsHook.js
@@ -6,16 +6,27 @@ export const usePokemon = (url) => {
     return axios.get(url).then(res => res.data)
   },{
     cacheTime:10000,
+    staleTime:10000,
     refetchOnWindowFocus:false
   })
   return pokemon;
 }
 
-export const usePokemons = () => {
-  const pokemons = useQuery("pokemons",() => {
-    return axios.get("https://pokeapi.co/api/v2/pokemon").then(res => res.data)
+export const usePokemons = (url) => {
+  const pokemons = useQuery(["pokemons",url],() => {
+    switch(url.status){
+      case "all":
+        return axios.get("https://pokeapi.co/api/v2/pokemon").then(res => res.data);
+      case "next":
+        return axios.get(url.next).then(res => res.data);
+      case "prev":
+        return axios.get(url.previous).then(res => res.data);
+      default:
+        return axios.get("https://pokeapi.co/api/v2/pokemon").then(res => res.data);
+    }
   },{
     cacheTime:10000,
+    staleTime:10000,
     refetchOnWindowFocus:false
   })
   return pokemons;
